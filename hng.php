4ea84101914097023efa137908c4c1d4131e3556
Ruqayyyah <38359391+Ruqayyyah@users.noreply.github.com>
2018-04-13 22:04:13 +0100

    Add files via upload

---
+++ hng.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset = "UTF-8">
+	<title>HNG stage1</title>
+	<link rel="stylesheet" type="text/css" href="hng.css">
+</head>
+<body>
+<div class="container">
+<h1> HNG INTERSHIP STAGE 1 CHALLENGE</h1>
+<p>designed by</p>
+<p>RUQAYYAH</p>
+	
+<div class="timebox">
+<?php 
+	date_default_timezone_set("Africa/Lagos");
+	echo "The time is ".date("h:i:sa");
+?>
+</div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
